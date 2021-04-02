3b408a764cefd73be0637e6ea506d8f913298c98
Halex Paiva <halexkmp@gmail.com>
2021-04-02 12:36:27 -0300

    adiçao URL redirect

--- usuario/urls.py
+++ usuario/urls.py
@@ -4,6 +4,7 @@ from usuario import views as viewsUser
 
 urlpatterns = [
    url(r'^login/$', views.login, {'template_name':'login.html'}, name='login'),
+   url(r'^accounts/login/$', views.login, {'template_name':'login.html'}, name='accounts-login'),
    url(r'^logout/$', views.logout_then_login, {'login_url':'/login/'}, name='logout'),
    url(r'^home/$', viewsUser.home, name='home'),
 ]
